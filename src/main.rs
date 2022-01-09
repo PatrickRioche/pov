8375f08fe8366bc45f32826b3bd0a7823da396c2
PatrickRioche/pov <patrick.rioche@gmail.com>
2022-01-09 10:50:43 +0100

    v1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+// pov V0.0					Angers, le 09/01/2022
+//
+//
+//
+//
+//#! [warn(dead_code)]
+
+use std::io;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+
+    //
+    //  Initialisation de variable
+    //
+	let mut compteur:i8 = 1;
+    
+    println!("POV:");
+    loop {
+
+		let mut input = String::new();
+
+		match io::stdin().read_line(&mut input) {
+			Ok(_n) => {
+				//println!("{} bytes read", n);
+				//println!("input ={}", input);
+			}
+			Err(error) => println!("error: {}", error),
+		};
+
+   		//
+		//	Test si le caractere q a ete saisir au clavier
+		//
+		for n in input.chars() {
+            //println!("{}",n);
+            if n == 'q' { process::exit(0); }
+        }
+        println!("compteur : {}", compteur);
+		compteur = compteur + 1;
+
+    }
 }
